03b1c64233bb3fb92d8b74c004771a4181be06e3
Gabriel Assys <engs-gabrieloliveira@camporeal.edu.br>
2025-01-07 11:19:43 -0300

    utilizando operações básicas

---
+++ 01_conceitos_basicos/operacoes_basicas.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# Criando dois arrays
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+# Operações matemáticas entre arrays
+print("Array A:", a)
+print("\nArray B:", b)
+print("\nSoma (A + B):", a + b)
+print("\nSubtração (A - B):", a - b)
+print("\nMultiplicação (A * B):", a * b)
+print("\nDivisão (A / B):", a / b)
+
+# Operações com escalares
+print("\nOperações com escalares:")
+print("A * 2:", a * 2)
+print("A + 10:", a + 10)
